283206537ff5433c7c4c5a3d6ec39d97d0b0f14d
nespolisimmaco <snespoli@yahoo.com>
2023-04-13 19:45:07 +0200

    Add: Main implemented

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,8 +10,23 @@ export default {
 </script>
 
 <template>
-    <h2>Main</h2>
+    <main>
+        <div class="container">
+            <h2>--> Content goes here &lt;--</h2>
+        </div>
+    </main>
 </template>
 
 <style scoped lang="scss">
+@use "../style/partials/mixins" as *;
+main {
+    background-color: black;
+    .container {
+        @include container();
+        padding: 2.5rem 0;
+        h2 {
+            color: white;
+        }
+    }
+}
 </style>
