6da6785943d84bec1a8f8b2d6d0730ab12542a37
nespolisimmaco <snespoli@yahoo.com>
2023-04-13 18:00:06 +0200

    Add: created header, main, and footer components

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
+</template>
+
+<style lang="scss">
+@use "./style/general.scss"
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: "AppMain",
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <h2>Main</h2>
+</template>
+
+<style scoped lang="scss">
+</style>
